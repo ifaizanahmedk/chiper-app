5f55a3757a031aaad1651c30da5f48db1b9a7987
ifaizanahmedk <faizanahmed497@gmail.com>
2023-01-15 04:35:34 +0500

    FEAT: Add Dashboard component to display tweets with ids with connect to chirper app.

---
+++ src/components/Dashboard.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+
+const Dashboard = (props) => {
+	return (
+		<div>
+			<h3 className="center">Your Timeline</h3>
+			<ul className="dashboard-list">
+				{props.tweetIds.map((id) => (
+					<li key={id}>
+						<div>Tweet ID: {id}</div>
+					</li>
+				))}
+			</ul>
+		</div>
+	);
+};
+
+const mapStateToProps = ({ tweets }) => ({
+	tweetIds: Object.keys(tweets).sort(
+		(a, b) => tweets[b].timestamp - tweets[a].timestamp
+	),
+});
+
+Dashboard.propTypes = {};
+
+export default connect(mapStateToProps)(Dashboard);
